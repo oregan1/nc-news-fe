381ed9a842bea792a0652adb79becfa33dd3d829
oregan1 <txmxregan@gmail.com>
2022-09-26 14:16:01 +0100

    Added list of all articles

---
+++ src/components/Articles/Articles.jsx
@@ -0,0 +1,34 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import ArticleCard from './ArticleCard';
+
+const Articles = () => {
+    const [articles, setArticles] = useState();
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        setIsLoading(true);
+        axios.get('https://tomnews.herokuapp.com/api/articles')
+        .then(({data}) => {
+            setArticles(data.articles);
+            setIsLoading(false)
+        })
+    },[]);
+
+    if(isLoading){
+        return <p>Loading articles...</p>
+    }else{
+        return <div>
+            <ul>
+                {articles.map((article) => {
+                    return <li key={article.article_id}>
+                        <ArticleCard article={article}/>
+                    </li>
+                })}
+            </ul>
+        </div>
+    }
+
+}
+
+export default Articles;
---
+++ src/components/Title/Title.jsx
@@ -0,0 +1,7 @@
+
+
+const Title = () => {
+    return <h1>TNC News</h1>
+}
+
+export default Title;
